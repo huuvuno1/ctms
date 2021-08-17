119804cfa112cf5ff2ee4ca81f618f184b91ca91
Nguyễn Hữu Vũ <nguyenhuuvuno1@gmail.com>
2021-08-17 08:59:11 +0700

    oke

--- README.md
+++
@@ -1,2 +0,0 @@
-
-<h1 style="color: red">This is an extension for vinlp</h1>
--- content.js
+++ content.js
@@ -1,132 +1,69 @@
-localStorage.setItem('fileNumber', 0);
-
-
-if (window.location.pathname == "/annotation") {
-    var boot = document.createElement('button');
-    boot.classList.add('btnvin');
-    boot.innerText = "fill";
-
-    var parent = document.querySelector('#main-segment-form > div > center:nth-child(1)');
-    boot.addEventListener('click', () => {
-        document.querySelector('#trans-dialect > option:nth-child(1)').selected = true;
-        document.querySelector('#trans-env > option:nth-child(1)').selected = true;
-        document.querySelector('#trans-age > option:nth-child(1)').selected = true;
-        document.querySelector('#trans-emotion > option:nth-child(1)').selected = true;
-    })
-    parent.appendChild(boot);
-
-    document.querySelector('#save-for-review').addEventListener('click', () => {
-        let fileNumber = localStorage.getItem('fileNumber');
-        localStorage.setItem('fileNumber', parseInt(fileNumber) + 1);
-    })
-    
-}
-
-
-document.querySelector('body').addEventListener('keydown', (e) => {
-    if (e.key == "Escape") {
-        var esc = document.querySelector("#close-segment-form");
-        esc.click();
-        esc = document.querySelector("#next-without-save");
-        esc.click();
-    }
-
-    if (e.key == 3) {
-        document.querySelector("#main-segment-form > div > center:nth-child(1) > button.btnvin").click();
-    }
-
-    if (e.key == 4) {
-        document.querySelector('#toggle-audio-btn').click();
-    }
-
-    if (e.key == 1) {
-        var audio = document.querySelector('#waveform > audio');
-        audio.currentTime -= 3;
-    }
-})
-
-
-if (window.location.pathname == "/review-annotation") {
-    
+if (location.pathname.includes("/DangkyLoptinchi.aspx")) {
     let button = document.createElement('button');
-    button.classList.add('btn', 'btn-sm', 'btn-danger');
-    button.innerText = 'Bắt đầu auto';
-    button.addEventListener('click', () => {
-        // open page check
-        document.querySelector('#btShowReviewDialog').click();
-        check()
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        let comfirm = confirm("CLick xong đợi nó load dữ liệu nhé, đừng click nhiều")
+        if (confirm) {
+            document.getElementById('leftcontent').innerHTML = "<img src='/images/ajaxbar.gif' alt='Đang tải dữ liệu' title='Đang tải dữ liệu' />"
+            location.href="javascript:getSubcribleModule(); void 0;";
+        }
     })
-    document.querySelector('body > div > div.dash-app > main > div > div.col-12 > div')
-        .appendChild(button)
-    
+    button.id = "nguyenhuuvu"
+    button.innerText = "Lấy nhanh danh sách môn";
+    document.querySelector("#topbackground").appendChild(button);
 }
 
 
 
-const check = async () => {
-
-    // wait load html    
-    await sleep(2000)
-    // find list check
-    let audios = document.querySelectorAll('#segments-table > tbody > tr')
-
-    if (!audios) {
-        return
-    }
-
-    let hasFileReject = false;
-
-    for (e of audios) {
-        e.querySelector('td').click();
-
-        // check xxx
-        let text = e.querySelector('#segments-table > tbody > tr.selected-row > td:nth-child(3)').innerText;
-
-        let eStatus = e.querySelector('td:nth-child(10)');
-        if (eStatus.innerText.includes('Không đạt')) {
-            console.log('detect reject ', document.querySelector('#review-title').innerText);
-            // click ignore
-            document.querySelector('#btSkipOver').click();
-            await sleep(2000);
-            document.querySelector('#btSkipOverGo').click();
-            check();
-            return;
+let s = document.createElement('script');
+s.textContent = `
+var tmp=0;
+function subcrible(loptcID, sinhvienID, ct) {
+    var args = "subcrible:" + loptcID+ ":" + sinhvienID;
+    var context = 'dvLopChoDangky';
+    tmp=sinhvienID;
+    WebForm_DoCallback('ctl00$LeftCol$LoptinchiDangky1',args,hienLoptinchi,context,null,false);
+    showWaiting(loptcID);
+    //remove the same subject rows
+    var trNodes = document.getElementsByTagName("tr");
+    var i=0;
+    while( i<trNodes.length){
+        if (trNodes[i].title!=null && trNodes[i].title==("m"+ct)){
+            trNodes[i].parentNode.removeChild(trNodes[i],true);
         }
-
-        if (text.includes('xxx')) {
-            hasFileReject = true;
-            document.querySelector('#btReject').click();
-        }
-        else
-            document.querySelector('#btApprove').click();
-        
-        console.log(text,"có chứa 'xxx' hay không: ", text.includes('xxx'))
-
-        let numRd = Math.floor(Math.random() * 3) + 2; 
-        await sleep(numRd * 1000);
+        else 
+            i++;
     }
-
-    // save
-    document.querySelector('#btSubmit').click();
-    await sleep(2000)
-
-    if (hasFileReject) {
-        console.log('click reject: ', document.querySelector('#review-title').innerText)
-        document.querySelector('#btRejectGo').click();
+}
+function unSubcrible(loptcID, sinhvienID){
+    var ans = confirm("Bạn có chắc chắn muốn huỷ đăng ký này?");
+    if(!ans) return;
+    var args = "unsubcrible:" + loptcID + ":" + sinhvienID;
+    var context = 'dvLopChoDangky';
+    tmp=sinhvienID;
+    WebForm_DoCallback('ctl00$LeftCol$LoptinchiDangky1',args,hienLoptinchi,context,null,false);
+    showWaiting(loptcID);
+}
+function hienLoptinchi(data, context){
+    if (data.indexOf("Lỗi: ")>=0){
+        alert(data);
+        return;
     }
-    else
-        document.querySelector('#btApproveGo').click();
-
-
-    // // update
-    chrome.extension.sendMessage({
-        type: 'done-check', 
-        data: {}
-    });
-
-    check();
+    var dv = document.getElementById(context);
+    dv.innerHTML = data;
+    if(context=='dvLopChoDangky')
+        getLopVuaDangky();
 }
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function getLopVuaDangky(){
+    var args = "list:" + tmp;
+    var context='dvLopVuaDangky';
+    WebForm_DoCallback('ctl00$LeftCol$LoptinchiDangky1',args,hienLoptinchi,context,null,false);
 }
+function showWaiting(lid){
+    var obj = document.getElementById('a' + lid);
+    var w = document.createElement('img');
+    w.setAttribute("src","/images/ajax.gif");
+    obj.parentNode.replaceChild(w, obj);
+}
+`
+document.querySelector('body').appendChild(s)
